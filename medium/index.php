1ab8859f6cd9d9cc2bea8da80bf4eea2d1bb6067
Nowal <nowalplayer@gmai.com>
2016-02-22 18:33:09 +0100

    Add index.php to all folders

---
+++ medium/index.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html prefix="www.ogp.me/#ns" class="no_js" lang="en">
+   <head>
+     <!-- Website's configuration -->
+     <meta http-equiv="Content-type" content="text/html;charset=utf-8">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge;chrome=1">
+     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+     <!-- Website's description -->
+     <title>JavaScript | Learning</title>
+     <meta name="author" content="Nowal">
+     <meta name="description" content="My mini-website for ...">
+     <meta name="keywords" content="javascript,js,code,php,html,css,fun">
+     <meta name="robots" content="noopd,noydir">
+     <!-- Website's links -->
+     <link rel="stylesheet" href="stylesheet/style.css" type="text/css">
+     <link rel="shortcut icon" href="web-gallery/favicon.ico" type="image/x-icon">
+     <script type="text/javascript" src="scripts/script.js"></script>
+     <!-- Social networks cards -->
+     <meta property="og:title" content="JavaScript | Learning">
+     <meta property="og:description" content="My mini-website ">
+     <meta property="og:url" content="http://mywebsite.com">
+     <meta property="og:image" content="web-gallery/og/image.png">
+     <meta property="twitter:card" content="summary">
+     <meta property="twitter:site" content="@itsNowal">
+     <meta property="twitter:title" content="JavaScript | Learning">
+     <meta property="twitter:description" content="My mini-website :-)">
+   </head>
+
+   <body>
+
+   </body>
+</html>
---
+++ small/index.php
@@ -0,0 +1,21 @@
+
+
+<!DOCTYPE html>
+<html prefix="www.ogp.me/#ns" class="no_js" lang="en">
+   <head>
+     <!-- Website's configuration -->
+     <meta http-equiv="Content-type" content="text/html;charset=utf-8">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge;chrome=1">
+     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+     <!-- Website's description -->
+     <title>JavaScript | Learning</title>
+     <!-- Website's links -->
+     <link rel="stylesheet" href="stylesheet/style.css" type="text/css">
+     <link rel="shortcut icon" href="web-gallery/favicon.ico" type="image/x-icon">
+     <script type="text/javascript" src="scripts/script.js"></script>
+   </head>
+
+   <body>
+
+   </body>
+</html>
